ff23e56e7915cc26f17949a13bcf10093719cd34
StAl15 <stihinaleksander@yandex.ru>
2023-09-11 19:53:21 +0500

    add some

--- src/components/Layouts/IndicatorsLayout/FiltersIndicators.tsx
+++ src/components/Layouts/IndicatorsLayout/FiltersIndicators.tsx
@@ -1,81 +1,73 @@
 import {FlexColumn} from "../../PrimaryComponents/FlexColumn";
 import {IconButton, MenuItem, Select, Typography} from "@mui/material";
 import {FlexRow} from "../../PrimaryComponents/FlexRow";
-import {FlexColumnStart} from "../../PrimaryComponents/FlexColumnStart";
 import Label from "../../PrimaryComponents/Label";
-import {DatePicker, LocalizationProvider} from "@mui/x-date-pickers-pro";
-import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import React from "react";
+import {CustomDateRangePicker} from "../../PrimaryComponents/CustomDateRangePicker";
 
 export const FiltersIndicators = () => {
+
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <FlexRow className={'w-full border-b-[1px] border-b-[#C7C7CF] pb-4 justify-between items-center'}>
 
-            <FlexRow className={'w-full border-b-[1px] border-b-[#C7C7CF] pb-4 justify-between items-center'}>
+            <FlexColumn className={'w-[16.25rem]'}>
+                <Typography fontWeight={800} fontSize={'1.5rem'} fontFamily={'mulish'}>НАЗВАНИЕ
+                    КОМПАНИИ</Typography>
+                <Typography color={'secondary.light'} fontSize={'1rem'} fontFamily={'mulish'}>сентября 2023
+                    года,
+                    вторник</Typography>
+            </FlexColumn>
 
-                <FlexColumn className={'w-[16.25rem]'}>
-                    <Typography fontWeight={800} fontSize={'1.5rem'} fontFamily={'mulish'}>НАЗВАНИЕ
-                        КОМПАНИИ</Typography>
-                    <Typography color={'secondary.light'} fontSize={'1rem'} fontFamily={'mulish'}>сентября 2023
-                        года,
-                        вторник</Typography>
+            <FlexRow className={'items-end space-x-4'}>
+                <CustomDateRangePicker/>
+                <FlexColumn>
+                    <Label>Отображение</Label>
+                    <Select
+                        sx={{backgroundColor: '#FFF'}}
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        value={0}
+                    >
+                        <MenuItem value={0}>По месяцам</MenuItem>
+                        <MenuItem value={1}>По дням</MenuItem>
+                        <MenuItem value={2}>По кварталам</MenuItem>
+                    </Select>
                 </FlexColumn>
-
-                <FlexRow className={'items-end space-x-4'}>
-                    <FlexColumnStart>
-                        <Label>Период</Label>
-                        <DatePicker sx={{backgroundColor: '#FFF'}}/>
-                    </FlexColumnStart>
-                    <FlexColumn>
-                        <Label>Отображение</Label>
-                        <Select
-                            sx={{backgroundColor: '#FFF'}}
-                            labelId="demo-simple-select-helper-label"
-                            id="demo-simple-select-helper"
-                            value={0}
-                        >
-                            <MenuItem value={0}>По месяцам</MenuItem>
-                            <MenuItem value={1}>По дням</MenuItem>
-                            <MenuItem value={2}>По кварталам</MenuItem>
-                        </Select>
-                    </FlexColumn>
-                    <Select
-                        sx={{backgroundColor: '#FFF'}}
-                        labelId="demo-simple-select-helper-label"
-                        id="demo-simple-select-helper"
-                        value={0}
-                    >
-                        <MenuItem value={0}>Юр.лица и счета</MenuItem>
-                        <MenuItem value={1}>По дням</MenuItem>
-                        <MenuItem value={2}>По кварталам</MenuItem>
-                    </Select>
-                    <Select
-                        sx={{backgroundColor: '#FFF'}}
-                        labelId="demo-simple-select-helper-label"
-                        id="demo-simple-select-helper"
-                        value={0}
-                    >
-                        <MenuItem value={0}>Проекты</MenuItem>
-                        <MenuItem value={1}>По дням</MenuItem>
-                        <MenuItem value={2}>По кварталам</MenuItem>
-                    </Select>
-                    <Select
-                        sx={{backgroundColor: '#FFF'}}
-                        labelId="demo-simple-select-helper-label"
-                        id="demo-simple-select-helper"
-                        value={0}
-                    >
-                        <MenuItem value={0}>Сделки</MenuItem>
-                        <MenuItem value={1}>По дням</MenuItem>
-                        <MenuItem value={2}>По кварталам</MenuItem>
-                    </Select>
-                    <IconButton className={'self-center'}>
-                        <img src={'images/cancel.svg'}/>
-                    </IconButton>
-                </FlexRow>
-
+                <Select
+                    sx={{backgroundColor: '#FFF'}}
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    value={0}
+                >
+                    <MenuItem value={0}>Юр.лица и счета</MenuItem>
+                    <MenuItem value={1}>По дням</MenuItem>
+                    <MenuItem value={2}>По кварталам</MenuItem>
+                </Select>
+                <Select
+                    sx={{backgroundColor: '#FFF'}}
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    value={0}
+                >
+                    <MenuItem value={0}>Проекты</MenuItem>
+                    <MenuItem value={1}>По дням</MenuItem>
+                    <MenuItem value={2}>По кварталам</MenuItem>
+                </Select>
+                <Select
+                    sx={{backgroundColor: '#FFF'}}
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    value={0}
+                >
+                    <MenuItem value={0}>Сделки</MenuItem>
+                    <MenuItem value={1}>По дням</MenuItem>
+                    <MenuItem value={2}>По кварталам</MenuItem>
+                </Select>
+                <IconButton className={'self-center'}>
+                    <img src={'images/cancel.svg'}/>
+                </IconButton>
             </FlexRow>
-        </LocalizationProvider>
 
+        </FlexRow>
     );
 };
